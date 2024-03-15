40458cce1b82c9a88fbaee04d7d89d5b991319aa
Alec Palo <alecpalo@Alecs-MacBook-Pro-4.local>
2024-03-15 14:43:20 -0400

    portfolio page

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -4,8 +4,10 @@ export default function Portfolio() {
     return(
         <main className={"bg-white"}>
             <div className={"h-32"}></div>
-            <div>
-
+            <div className={"text-gray-800 w-full text-center pb-10"}>
+                <h1 className={"text-2xl"}>
+                    Maybe some quote about something IDK
+                </h1>
             </div>
             <div className={"w-full h-full flex flex-wrap justify-center"}>
                 <PropertyHero/>
